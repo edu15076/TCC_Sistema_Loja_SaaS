a0e4735f5e8edf8fed2deb7aab9b0d4a03154f46
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-05-28 16:19:45 -0300

    :lispstick: style: formatação de arquivos da interface

--- src/sistema_loja_saas/urls.py
+++ src/sistema_loja_saas/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from saas.views.a import A
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('a/', A.as_view()),
 ]
