8d5fb33cb1c519616f524111cee164322f5bdbc3
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-10-28 23:59:52 -0300

    🚧 abc: correção de interface para ser implementada com HTMX

--- src/saas/views/interfaces/gestao_contratos.py
+++ src/saas/views/interfaces/gestao_contratos.py
@@ -5,11 +5,11 @@ from django.views.generic import ListView, CreateView
 from django.views.generic.edit import DeletionMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from util.views.edit_list import CreateOrUpdateListView
+from util.views.edit_list import CreateOrUpdateListHTMXView
 
 
 class ABCGestaoContratoCRUDListView(
-    ABC, LoginRequiredMixin, CreateOrUpdateListView, DeletionMixin
+    ABC, LoginRequiredMixin, CreateOrUpdateListHTMXView, DeletionMixin
 ):
     @abstractmethod
     def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
@@ -22,11 +22,11 @@ class ABCGestaoContratoCRUDListView(
         - `filtro`: um valor que informa se será exibido os contratos ativos 
             ou inativos
         ? - `query`: talvez tenha como pesquisar pelo possivel titulo de um contrato
-        ? - `pagina`: talvez seja bom fazer paginação.
+        - `page`: retorna a respectiva pagina.
 
         :type request: HttpRequest
         :return: um `context` contendo:
-            - `usuario`
+            // - `usuario`
             - `contratos`:
                 - ...
             - `form`: formulário de cadastro de contratos
@@ -41,7 +41,7 @@ class ABCGestaoContratoCRUDListView(
         atualizações)
 
         :param request: deve conter os seguintes campos:
-        - `action`: informa a ação realizada;
+        //- `action`: informa a ação realizada;
 
         pode conter os seguintes campos:
 
