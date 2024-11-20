4707f5501eb293ce2caed8fad44f49bfdf074fa7
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-11-20 16:44:22 -0300

    :sparkles: feat: modelos de produto, produto por lote e promoção criados

---
+++ src/loja/models/produto.py
@@ -0,0 +1,76 @@
+from datetime import datetime
+
+from django.db import models
+from django.db.models import F
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator
+
+from util.mixins import ValidateModelMixin
+from loja.models import Loja, Promocao
+
+
+__all__ = ['Produto', 'ProdutoPorLote']
+
+
+class ProdutoQuerySet(models.QuerySet):
+    pass
+
+
+class ProdutoManager(models.Manager):
+    def get_queryset(self):
+        return ProdutoQuerySet(self.model, using=self._db).all()
+
+
+class Produto(ValidateModelMixin, models.Model):
+    preco_de_venda = models.DecimalField(
+        _('Preço de venda'), 
+        max_digits=11, 
+        decimal_places=2,
+        validators=[MinValueValidator(0, _('Preço não pode ser negativo.'))]
+    )
+    codigo_de_barras = models.CharField(_('Código de barras'), max_length=128, blank=True)
+    em_venda = models.BooleanField(_('Disponível para venda'), default=False)
+    loja = models.ForeignKey(Loja, verbose_name=_('Loja'), on_delete=models.RESTRICT, editable=False)
+
+    produtos = ProdutoManager()
+
+    @property
+    def qtd_em_estoque(self):
+        return sum([p.qtd_em_estoque for p in self.lotes.produtos_por_lote.filter(produto=self)])
+
+    def promocao_por_data(self, data: datetime) -> 'Promocao' | None:
+        """
+        Retorna a promoção ativa para a data passada.
+        """
+
+        promocao = self.promocoes.filter(
+            data_inicio__lte=data
+        ).annotate(
+            tempo_total=F('periodo__tempo_total')
+        ).filter(
+            tempo_total__gte=(data - F('data_inicio'))
+        ).first()
+
+        return promocao if promocao else None
+    
+    def promocao_ativa(self) -> 'Promocao' | None:
+        """
+        Retorna a promoção ativa para a data atual.
+        """
+        return self.promocao_por_data(datetime.now())
+
+class ProdutoPorLoteQuerySet(models.QuerySet):
+    pass
+
+
+class ProdutoPorLoteManager(models.Manager):
+    def get_queryset(self):
+        return ProdutoPorLoteQuerySet(self.model, using=self._db).all()
+
+
+class ProdutoPorLote(models.Model):
+    lote = models.CharField(_('Lote'), max_length=128)
+    qtd_em_estoque = models.IntegerField(_('Quantidade'), validators=[MinValueValidator(0, _('Quantidade não pode ser negativo.'))])
+    produto = models.ForeignKey(Produto, verbose_name=_('Produto'), on_delete=models.RESTRICT, editable=False, related_name='lotes')
+
+    produtos_por_lote = ProdutoPorLoteManager()
---
+++ src/loja/models/promocao.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from common.models.periodo import Periodo
+from django.forms.models import model_to_dict
+
+from util.mixins import ValidateModelMixin, NotUpdatableFieldMixin
+from loja.models import Loja, Produto
+
+
+class PromocaoQuerySet(models.QuerySet):
+    pass
+
+
+class PromocaoManager(models.Manager):
+    def get_queryset(self):
+        return PromocaoQuerySet(self.model, using=self._db).all()
+    
+
+class Promocao(ValidateModelMixin, models.Model):
+    porcentagem_desconto = models.IntegerField(
+        _('Porcentagem do desconto'),
+        blank=False,
+        validators=[
+            MaxValueValidator(100, _('Porcentagem não pode exceder 100%.')),
+            MinValueValidator(0, _('Porcentagem não pode ser negativo.')),
+        ]
+    )
+    data_inicio = models.DateField(_('Data de início'), blank=False)
+    descricao = models.CharField(_('Descrição'), max_length=246, blank=True)
+    periodo = models.ForeignKey(Periodo, verbose_name=_('Período'), on_delete=models.RESTRICT)
+    loja = models.ForeignKey(Loja, verbose_name=_('Loja'), on_delete=models.RESTRICT, editable=False)
+    produtos = models.ManyToManyField(Produto, verbose_name=_('Produtos'), related_name='promocoes')
+
+    promocoes = PromocaoManager()
+
+    def clonar_promocao(self, data_inicio: datetime) -> 'Promocao':
+        """
+        Clona a promoção atual com a data de início passada.
+        """
+        promocao = Promocao(
+            porcentagem_desconto=self.porcentagem_desconto,
+            data_inicio=data_inicio,
+            descricao=self.descricao,
+            periodo=self.periodo,
+            loja=self.loja
+        )
+        promocao.save()
+        return promocao
