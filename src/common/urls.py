90c9f1e147fa01a5d77100c37a0ddbe5b5dc2c38
El0y-C0SM0 <eloymaciel06@gmail.com>
2024-11-20 11:59:53 -0300

    :sparkles: feat: pagina de acesso não autorizado

--- src/common/urls.py
+++ src/common/urls.py
@@ -3,4 +3,10 @@ from django.views.generic import TemplateView
 
 from .views import *
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        'acesso-nao-autorizado/',
+        TemplateView.as_view(template_name="acesso_nao_autorizado.html"),
+        name='acesso_nao_autorizado',
+    ),
+]
