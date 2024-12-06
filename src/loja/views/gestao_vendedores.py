b0f1d5d1215fc3c485510482dfd78a4e6e33e782
Eduardo Costa de Souza <eduardbh2014@gmail.com>
2024-12-05 22:34:38 -0300

    :sparkles: feat: Adição de verificação de permissão nas telas de gestão de vendedores

--- src/loja/views/gestao_vendedores.py
+++ src/loja/views/gestao_vendedores.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import HttpResponseNotFound, JsonResponse
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, TemplateView, FormView
@@ -37,15 +38,16 @@ class VendedorContextDataMixin:
 
 
 class ListVendedoresView(
-    LojaProtectionMixin, VendedorContextDataMixin, HTMXHelperMixin, ListView
+    LojaProtectionMixin, PermissionRequiredMixin, VendedorContextDataMixin, HTMXHelperMixin, ListView
 ):
     template_name = 'gestao_vendedores/includes/list_vendedores.html'
     model = Vendedor
-    # restrict_direct_access = True
+    restrict_direct_access = True
     context_object_name = 'vendedores'
     ordering = ['-is_active', '-_porcentagem_comissao', 'nome']
 
     usuario_class = GerenteDeRH
+    permission_required = 'loja.gerir_vendedores'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -58,13 +60,15 @@ class ListVendedoresView(
 
 
 class CardVendedorView(
-    LojaProtectionMixin, VendedorContextDataMixin, HTMXHelperMixin, DetailView
+    LojaProtectionMixin, PermissionRequiredMixin, VendedorContextDataMixin, HTMXHelperMixin, DetailView
 ):
     template_name = 'gestao_vendedores/cards/card_vendedor.html'
     model = Vendedor
     context_object_name = 'vendedor'
+    restrict_direct_access = True
 
     usuario_class = GerenteDeRH
+    permission_required = 'loja.gerir_vendedores'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -76,7 +80,7 @@ class CardVendedorView(
 
 
 class AlterarComissaoVendedorView(
-    LojaProtectionMixin, HTMXFormMixin, FormView
+    LojaProtectionMixin, PermissionRequiredMixin, HTMXFormMixin, FormView
 ):
     form_class = AlterarComissaoVendedorForm
     form_template_name: str = (
@@ -84,11 +88,13 @@ class AlterarComissaoVendedorView(
     )
     template_name = 'gestao_vendedores/forms/htmx_alterar_comissao_vendedor.html'
     redirect_on_success = False
+    restrict_direct_access = True
     success_url = None
     hx_target_form_invalid = 'this'
     hx_swap_form_invalid = 'outerHTML'
 
     usuario_class = GerenteDeRH
+    permission_required = 'loja.gerir_vendedores'
 
     def get_context_data(self, **kwargs):
         return super().get_context_data(**kwargs) | {
@@ -115,9 +121,6 @@ class AlterarComissaoVendedorView(
             setattr(self, 'form', super().get_form(form_class))
         return self.form
 
-    def form_invalid(self, form):
-        return super().form_invalid(form)
-
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
@@ -127,11 +130,12 @@ class AlterarComissaoVendedorView(
 
 
 class GestaoVendedoresView(
-    UserFromLojaRequiredMixin, TemplateView
+    UserFromLojaRequiredMixin, PermissionRequiredMixin, TemplateView
 ):
     template_name = 'gestao_vendedores/gestao_vendedores.html'
 
     usuario_class = GerenteDeRH
+    permission_required = 'loja.gerir_vendedores'
 
     def get_login_url(self):
         return reverse('login_loja', kwargs={'loja_scope': int(self.scope)})
